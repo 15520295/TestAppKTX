2644db74edb8fa101d9aeb1ca764ec790d19f968
HuyDaoDuc <15520295>
2018-03-13 13:20:02 +0700

    Test git Success

--- app/src/main/java/com/example/huydaoduc/testappktx/MapsActivity.java
+++ app/src/main/java/com/example/huydaoduc/testappktx/MapsActivity.java
@@ -11,6 +11,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 // test clone
+//Test success
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
